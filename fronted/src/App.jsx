14a4b2f50a247f4838208296bcb2c1c617966e1a
parajuli rahul <parajulirahul98@gmail.com>
2023-09-07 00:26:23 +0545

    content add

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -10,9 +10,9 @@ const App = () => {
    <Routes>
     <Route path='/' element= {<Home/>}/>
     <Route path='/books/create' element= {<CreateBooks/>}/>
-    <Route path='/book/details/:id' element= {<ShowBook/>}/>
-    <Route path='/book/edit/:id' element= {<EditBook/>}/>
-    <Route path='/book/delete/:id' element= {<DeleteBook/>}/>
+    <Route path='/books/details/:id' element= {<ShowBook/>}/>
+    <Route path='/books/edit/:id' element= {<EditBook/>}/>
+    <Route path='/books/delete/:id' element= {<DeleteBook/>}/>
 
 
 
